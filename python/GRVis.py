6ff3062436ca0883fe2c893a2de5b5c39fc9662f
milindasf <milinda@cs.utah.edu>
2018-04-02 08:20:19 -0600

    initial vtk python files created.

---
+++ python/GRVis.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#####
+### CS6635 - Scientific Visualization Project. VTK based visualization framework for computational relativity. 
+### Author: Milinda Fernando, Max Carlson
+### School of Computing, University of Utah.
+### Date: 04/02/2018
+####
+
+'''
+main python file for GR Visualization. 
+
+'''
+
+
+def main():
+        
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ python/filters.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#####
+### CS6635 - Scientific Visualization Project. VTK based visualization framework for computational relativity. 
+### Author: Milinda Fernando, Max Carlson
+### School of Computing, University of Utah.
+### Date: 04/02/2018
+####
+
+'''
+
+All the filters, and filter helper functions. 
+
+'''
