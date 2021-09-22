cba4a554a1dc9e3f00465699c255972ec4032e75
Milinda Shayamal Fernando <milinda@cs.utah.edu>
2021-09-22 15:03:31 -0500

    Update GRVis.py

--- python/GRVis.py
+++ python/GRVis.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-#####
-### CS6635 - Scientific Visualization Project. VTK based visualization framework for computational relativity. 
-### Author: Milinda Fernando, Max Carlson
-### School of Computing, University of Utah.
-### Date: 04/02/2018
-####
-
 '''
 main python file for GR Visualization. 
 
